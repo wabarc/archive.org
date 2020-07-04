508423070565ecef61843e02f957b8b876cd5e0a
nobody <noreply@github.com>
2020-07-05 00:12:18 +0800

    Update test

--- main_test.go
+++ main_test.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	"github.com/wabarc/archive.org/pkg"
 	"strings"
 	"testing"
+
+	"github.com/wabarc/archive.org/pkg"
 )
 
 func TestWayback(t *testing.T) {
 	url := "https://www.google.com"
 	links := []string{url}
-	got := ia.Wayback(links)
-	s := strings.Join(got, " ")
-	if strings.Contains(s, url) == false || strings.Contains(s, "archive.org") == false {
-		t.Error(got)
-		t.Fail()
+	wbrc := &ia.Archiver{}
+	got, _ := wbrc.Wayback(links)
+	for _, dest := range got {
+		if strings.Contains(dest, url) == false || strings.Contains(dest, "archive.org") == false {
+			t.Error(got)
+			t.Fail()
+		}
 	}
 }
