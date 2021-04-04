64a445aa760cf423613ec2aebefe6e52ffb8380b
nobody <noreply@github.com>
2021-04-04 07:31:13 +0000

    Minor improvements

--- pkg/ia.go
+++ pkg/ia.go
@@ -1,6 +1,7 @@
 package ia
 
 import (
+	"fmt"
 	"log"
 	"sync"
 
@@ -9,7 +10,7 @@ import (
 
 // Wayback is the handle of saving webpages to archive.org
 func (wbrc *Archiver) Wayback(links []string) (map[string]string, error) {
-	collect := make(map[string]string)
+	collect, results := make(map[string]string), make(map[string]string)
 	for _, link := range links {
 		if !helper.IsURL(link) {
 			log.Print(link + " is invalid url.")
@@ -21,16 +22,23 @@ func (wbrc *Archiver) Wayback(links []string) (map[string]string, error) {
 	ch := make(chan string, len(collect))
 	defer close(ch)
 
+	var mu sync.Mutex
 	var wg sync.WaitGroup
 	for link := range collect {
 		wg.Add(1)
 		go func(link string) {
 			wbrc.fetch(link, ch)
-			collect[link] = <-ch
+			mu.Lock()
+			results[link] = <-ch
+			mu.Unlock()
 			wg.Done()
 		}(link)
 	}
 	wg.Wait()
 
-	return collect, nil
+	if len(results) == 0 {
+		return results, fmt.Errorf("No results")
+	}
+
+	return results, nil
 }
